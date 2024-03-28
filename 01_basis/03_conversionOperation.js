3926cadfa7be21b115502a43bc71d46ecde120ba
Amarnath Garai <amarnathgarai2004@gmail.com>
2024-03-28 23:53:02 +0530

    Operations

--- 01_basis/03_conversionOperation.js
+++ 01_basis/03_conversionOperation.js
@@ -11,9 +11,44 @@
 // "33abc" => NaN
 // true =>1, false =>0
 
-let isLoggedIn = "hehe"
+// let isLoggedIn = "hehe"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
+// ********** Operations ***********//
+
+// let value = 3
+
+// let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2+9);
+// console.log(2**9);
+
+let str1 = "hello"
+
+let str2="Amarnath"
+
+let str3=str1+str2
+
+console.log(str3)
+
+console.log('3'+3+3+5)
+
+console.log(4+4+"4 ")
+
+console.log(+"")
+console.log(+true)
+
+let num1 , num2,num3
+
+num1 = num2=num3 = 2+2
+
+let gameCounter = 100
+console.log(num1)
+gameCounter++
+
+console.log(gameCounter)
