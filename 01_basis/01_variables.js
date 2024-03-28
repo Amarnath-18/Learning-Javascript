ff019bb995c35bc0774c8ebf921f62e55548e7d5
Amarnath Garai <amarnathgarai2004@gmail.com>
2024-03-28 22:54:58 +0530

    dataTypes

---
+++ 01_basis/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144722
+
+let accountEmail="dear@gmail.com"
+
+var accountPassword = "passWord"
+accountCity = "West Bengal"
+// accountId = 2
+/*
+Prefer not to use var
+*/
+let accountState;
+accountPassword = "bari bari"
+
+console.log(accountPassword)
+console.log(accountState)
+console.table([accountEmail , accountId , accountPassword , accountCity])
---
+++ 01_basis/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat all JS code as newer version
+
+// alert("hello") // we are using nodejs not browser
+
+console.log(8+9)
+console.log("Amarnath")
+
+let name = "Amarnath"
+let age = 18
+
+let isPresent = false
+
+console.log(typeof(isPresent))
+
+//number => 2 to  power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+
+// undefined =>
+// symbol => unique
+
+
+//object
+
+console.log(typeof null)
+
