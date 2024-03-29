b0eea9a9c37502613f76b02808e3c6fd6600ee7f
Amarnath Garai <amarnathgarai2004@gmail.com>
2024-03-29 09:49:18 +0530

    change in location

---
+++ 01_basis/datatypes-summary.js
@@ -0,0 +1,18 @@
+// Primitive
+// 7 types : String , Number , Boolean , null , undefined , Symbol , Bigint
+
+// Reference (Non primitive)
+
+// Array , Objects , Functions
+
+const id = Symbol('123')
+
+const id2 = Symbol('123')
+
+console.log(id === id2)
+
+console.log(id , id2)
+
+const bigNumber = 9827352857n
+
+console.log(typeof bigNumber)
