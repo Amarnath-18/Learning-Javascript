f098d8bc8ae0f73bddc72b705f9ebdba61e64df1
Amarnath Garai <amarnathgarai2004@gmail.com>
2024-03-31 18:54:54 +0530

    Update all string methods

---
+++ 01_basis/05_Strings.js
@@ -0,0 +1,23 @@
+const Name = "Amarnath"
+const repoCount = 90
+
+console.log(`Hello My name is ${Name} and my repo count is ${repoCount}`);
+
+// - strings can be donated by ' or "
+// - to concatenate we can use 
+// - back tick (sting interpolations)
+// e.g `hello ${name}`
+// - sting is object but it has length property
+// - it can be access as
+// e.g stringName[0]
+// - stringName._proto_
+// - stringName.toUpperCase()
+// - stringName.charAt()
+// - stringName.indexAt()
+// - stringName.substing(0,4) can have -ve value
+// - stringName.slice(-7,4) can have -ve value
+// - stringName.trim() , .trimStart(), .trimEnd()
+// - stringName.replace('what to search', 'what to replace with')
+// - stringName.includes('name')
+// - stringName.split('sepater','limit')
+// - search for small() yourself
