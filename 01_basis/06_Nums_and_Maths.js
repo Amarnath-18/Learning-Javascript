914e3e545ab9f56f355d91b0eca07de2dc2c8ada
Amarnath Garai <amarnathgarai2004@gmail.com>
2024-04-01 09:16:52 +0530

    nums and maths

---
+++ 01_basis/06_Nums_and_Maths.js
@@ -0,0 +1,19 @@
+// const score = 'a'
+
+// console.log(typeof score)
+// const balance = new Number(122.44)
+
+// console.log(typeof balance.toString(), balance.toFixed(9))
+// console.log(Math);
+
+// console.log(Math.abs(-332));
+
+// console.log(Math.round(4.49),Math.ceil(4.7),Math.floor(4.7));
+
+console.log((Math.random()*10) +1);
+
+const min = 10
+
+const max =20
+
+console.log(Math.floor(Math.random() * (max - min + 1) ) + min)
