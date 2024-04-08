5b7fb04e1d17d54a18183ec859cbf93d69f7f715
Amarnath Garai <amarnathgarai2004@gmail.com>
2024-04-08 23:08:59 +0530

    array 2

---
+++ 02_basis/02_Arrays2.js
@@ -0,0 +1,8 @@
+let arr1=["apple", "mango" , " bananna"]
+let arr2 = ["kola" , "aamm" , "lebu"]
+
+let arr3 = arr1.concat(arr2)  //arr1 + arr2
+// arr1.push(arr2)
+// console.log(arr3);
+let all_arr = [...arr1,...arr2]
+console.log(all_arr);
